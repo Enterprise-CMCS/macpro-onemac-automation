c6207d0afce1c3f2f059872f862acd805d5c9efd
Ammar <testautomationnotifications@gmail.com>
2026-02-12 16:11:43 -0500

    adding json report

--- src/main/java/gov/cms/onemac/utils/ExtentReportManager.java
+++ src/main/java/gov/cms/onemac/utils/ExtentReportManager.java
@@ -2,19 +2,26 @@ package gov.cms.onemac.utils;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-
-import java.util.TimeZone;
+import com.aventstack.extentreports.reporter.JsonFormatter;
 
 public class ExtentReportManager {
     private static ExtentReports extent;
 
     public static ExtentReports getInstance() {
         if (extent == null) {
+            extent = new ExtentReports();
+
+            // 1. Setup HTML Report (Spark)
             ExtentSparkReporter spark = new ExtentSparkReporter("extent-report/OneMACTestReport.html");
             spark.config().setReportName("OneMAC Automated Test Report");
             spark.config().setDocumentTitle("Test Results");
-            extent = new ExtentReports();
-            extent.attachReporter(spark);
+
+            // 2. Setup JSON Report
+            // In 5.1.1, the class is JsonFormatter
+            JsonFormatter json = new JsonFormatter("extent-report/OneMACTestReport.json");
+
+            // 3. Attach both reporters
+            extent.attachReporter(spark, json);
         }
         return extent;
     }
